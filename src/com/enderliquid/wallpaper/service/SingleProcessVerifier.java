7ab95950fd45a27e240c54169fbc65edf7e4e287
EnderLiquid <3546817839@qq.com>
2023-08-30 08:30:42 +0800

    fix exit on reload

--- src/com/enderliquid/wallpaper/service/SingleProcessVerifier.java
+++ src/com/enderliquid/wallpaper/service/SingleProcessVerifier.java
@@ -30,32 +30,32 @@ public class SingleProcessVerifier {
                     globalLogger.warning("关闭socket时出现异常" + Utility.exceptionDetailsOf(e));
                 }
             }
-        }
-        globalLogger.info("开始监听端口：" + port);
-        try {
-            server = new ServerSocket(port);
-            globalLogger.info("成功监听端口：" + port);
-            handleThread = new Thread(() -> {
-                while (!Thread.interrupted()) {
-                    try {
-                        server.accept();
-                        globalLogger.info("成功响应连接请求");
-                        Controller.setOnTop();
-                    } catch (IOException e) {
-                        globalLogger.warning("等待连接时出现异常，可能socket已被关闭" + Utility.exceptionDetailsOf(e));
+            globalLogger.info("开始监听端口：" + port);
+            try {
+                server = new ServerSocket(port);
+                globalLogger.info("成功监听端口：" + port);
+                handleThread = new Thread(() -> {
+                    while (!Thread.interrupted()) {
+                        try {
+                            server.accept();
+                            globalLogger.info("成功响应连接请求");
+                            Controller.setOnTop();
+                        } catch (IOException e) {
+                            globalLogger.warning("等待连接时出现异常，可能socket已被关闭" + Utility.exceptionDetailsOf(e));
+                        }
                     }
+                });
+                handleThread.start();
+            } catch (IOException e) {
+                globalLogger.severe("端口已被占用：" + port + Utility.exceptionDetailsOf(e));
+                globalLogger.info("正在连接到原有进程的socket");
+                try (Socket client = new Socket(HOST, port)) {
+                    globalLogger.info("连接成功");
+                } catch (IOException ex) {
+                    globalLogger.warning("连接时出现异常" + Utility.exceptionDetailsOf(ex));
                 }
-            });
-            handleThread.start();
-        } catch (IOException e) {
-            globalLogger.severe("端口已被占用：" + port + Utility.exceptionDetailsOf(e));
-            globalLogger.info("正在连接到原有进程的socket");
-            try (Socket client = new Socket(HOST, port)) {
-                globalLogger.warning("连接成功");
-            } catch (IOException ex) {
-                globalLogger.warning("连接时出现异常" + Utility.exceptionDetailsOf(ex));
+                WallpaperManager.exit(-1);
             }
-            WallpaperManager.exit(-1);
         }
     }
 }
